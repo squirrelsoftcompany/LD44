a19274b7f91c838cc0251e8dc55c2177c5949b5e
OroshiX <oroshix@users.noreply.github.com>
2019-04-27 12:27:48 +0200

    [FCT] Cinemachine: camera following hero (and movement dummy to remove)

---
+++ Assets/Scripts/MoveDummy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EventSystem;
+using UnityEngine;
+
+public class MoveDummy : MonoBehaviour {
+    [SerializeField] private GameEvent pickupObj;
+
+    private float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.name.Equals("Wall")) {
+            pickupObj.Raise();
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, 0f,
+            Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
+    }
+}
