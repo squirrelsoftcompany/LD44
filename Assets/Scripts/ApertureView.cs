7000b00be37518b254be035f5ce2368afbabdca5
Xavier Chalut <xavier.chalut@gmail.com>
2019-04-28 21:47:38 +0200

    [FCT] Add  basic fade system to avoid visual obstruction

---
+++ Assets/Scripts/ApertureView.cs
@@ -0,0 +1,101 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class ApertureView :
+    MonoBehaviour
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+    #endregion
+    #region Constants
+    /***************************************************/
+    /***  CONSTANTS             ************************/
+    /***************************************************/
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    public GameObject m_hero;
+    public float m_radius;
+
+    private AudioSource m_ambientSound = new AudioSource();
+
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 l_middle = m_hero.transform.position;
+        Vector3 l_top = new Vector3(m_hero.transform.position.x + m_radius, m_hero.transform.position.y, m_hero.transform.position.z);
+        Vector3 l_bottom = new Vector3(m_hero.transform.position.x - m_radius, m_hero.transform.position.y, m_hero.transform.position.z);
+        Vector3 l_left = new Vector3(m_hero.transform.position.x, m_hero.transform.position.y, m_hero.transform.position.z + m_radius);
+        Vector3 l_right = new Vector3(m_hero.transform.position.x, m_hero.transform.position.y, m_hero.transform.position.z - m_radius);
+
+        OccultingObjects(this.transform.position, l_middle);
+        OccultingObjects(this.transform.position, l_top);
+        OccultingObjects(this.transform.position, l_bottom);
+        OccultingObjects(this.transform.position, l_left);
+        OccultingObjects(this.transform.position, l_right);
+    }
+
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    void OccultingObjects(Vector3 p_source, Vector3 p_destination, int p_layerMask = Physics.DefaultRaycastLayers)
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(p_source, p_destination - p_source);
+
+        Debug.DrawLine(p_source, p_destination,Color.red);
+        foreach (RaycastHit l_hit in hits)
+        {
+            if(l_hit.transform.gameObject.GetComponent<DynamiqueOpacity>())
+            {
+                l_hit.transform.gameObject.GetComponent<DynamiqueOpacity>().FadeOutOpacity(0.5f, 0.5f);
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/DynamiqueOpacity.cs
@@ -0,0 +1,131 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class DynamiqueOpacity :
+    MonoBehaviour
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+    #endregion
+    #region Constants
+    /***************************************************/
+    /***  CONSTANTS             ************************/
+    /***************************************************/
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+    
+    private float m_initialOpacity;
+    private bool m_modifiedOpacity = false;
+    private bool m_modifiedRequested = false;
+    private float m_fadeTime = 0.0f;
+    private float m_fadeTimer = 0.0f;
+    private float m_opacityGoal;
+
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        if (transform.GetComponent<Renderer>())
+        {
+            m_initialOpacity = transform.GetComponent<Renderer>().material.color.a;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(m_modifiedOpacity)
+        {
+            Color col = transform.GetComponent<Renderer>().material.color;
+            if (col != null)
+            {
+                //float l_progressTime =  Mathf.Lerp(m_fadeTime, 0.0f, m_fadeTimer);
+                float l_progressTime;
+                if (m_fadeTimer > 0 )
+                {
+                    l_progressTime =  (m_fadeTime - m_fadeTimer) / m_fadeTime;
+                }
+                else
+                {
+                    l_progressTime = 1.0f;
+                }
+                Debug.Log("l_progressTime : " + l_progressTime);
+                col.a = Mathf.Lerp(m_initialOpacity, m_opacityGoal, l_progressTime);
+                Debug.Log(col.a);
+                //col.a = Mathf.Lerp(m_initialOpacity, m_opacityGoal, l_progressTime) * 0.01f;
+                transform.GetComponent<Renderer>().material.color = col;
+            }
+        }
+        
+        m_fadeTimer -= Time.deltaTime;
+        if (m_fadeTimer < 0)
+        {
+            if(!m_modifiedRequested)
+            {
+                m_modifiedOpacity = false;
+                //Restoring opacity directly (no fading yet...)
+                Color l_col = transform.GetComponent<Renderer>().material.color;
+                l_col.a = m_initialOpacity;
+                transform.GetComponent<Renderer>().material.color = l_col;
+            }
+
+        }
+        m_modifiedRequested = false;
+    }
+
+
+    /********  PUBLIC           ************************/
+
+    public void FadeOutOpacity(float p_fadeTime, float p_opacity)
+    {
+        m_modifiedRequested = true;
+        if (!m_modifiedOpacity)
+        {
+            m_fadeTimer = p_fadeTime;
+            m_fadeTime = p_fadeTime;
+            m_opacityGoal = p_opacity;
+            m_modifiedOpacity = true;
+        }
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+
+    #endregion
+}
