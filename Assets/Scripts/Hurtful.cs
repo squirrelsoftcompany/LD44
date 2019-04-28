826e5b438c9181e8de03bc07365218de440ca6d8
OroshiX <oroshix@users.noreply.github.com>
2019-04-28 18:20:09 +0200

    [FCT] Laser!

--- Assets/Scripts/Hurtful.cs
+++ Assets/Scripts/Hurtful.cs
@@ -1,11 +1,17 @@
+using System;
 using behavior;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider))]
 public class Hurtful : MonoBehaviour {
     [SerializeField] private float hurtAmount;
+    private const int maxParticules = 10;
 
     public void hurtMe(Life life) {
         life.lose(hurtAmount);
+    }
+
+    public void hurtMe(Life life, int nbParticules) {
+        var part = Math.Min(nbParticules, maxParticules);
+        life.lose(hurtAmount * part);
     }
 }
--- Assets/Scripts/obstacles/Laser.cs
+++ Assets/Scripts/obstacles/Laser.cs
@@ -1,26 +1,44 @@
-using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using behavior;
 using UnityEngine;
 
 namespace obstacles {
     [RequireComponent(typeof(LineRenderer))]
     public class Laser : MonoBehaviour {
         private LineRenderer lineRenderer;
-        private bool firing = true;
+        [SerializeField] private float hurtAmount = 1f;
+        private Dictionary<Life, float> hitObjects;
+        [SerializeField] private float hitRate = 0.5f;
 
         private void Awake() {
             lineRenderer = GetComponent<LineRenderer>();
+            lineRenderer.enabled = true;
+            hitObjects = new Dictionary<Life, float>();
         }
 
-        IEnumerator fireLaser() {
-            lineRenderer.enabled = true;
-            while (firing) {
-                var ray = new Ray(transform.position, transform.forward);
-                lineRenderer.SetPosition(0, ray.origin);
-                lineRenderer.SetPosition(1, ray.GetPoint(100));
-                yield return null;
+        private void Update() {
+            
+            foreach (var key in hitObjects.Keys.ToList()) {
+                hitObjects[key] = hitObjects[key] - Time.deltaTime;
+                if (hitObjects[key] < 0) {
+                    hitObjects.Remove(key);
+                }
             }
 
-            lineRenderer.enabled = false;
+            var ray = new Ray(transform.position, transform.forward);
+            lineRenderer.SetPosition(0, ray.origin);
+            if (Physics.Raycast(ray, out var hit, 100)) {
+                lineRenderer.SetPosition(1, hit.point);
+                var hitLife = hit.transform.GetComponent<Life>();
+
+                if (hitLife && !hitObjects.ContainsKey(hitLife)) {
+                    hitObjects[hitLife] = hitRate;
+                    hitLife.lose(hurtAmount);
+                }
+            } else {
+                lineRenderer.SetPosition(1, ray.GetPoint(100));
+            }
         }
     }
 }
