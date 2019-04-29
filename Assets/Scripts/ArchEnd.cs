d65cf7a5feadd43bef2cf2007101b9b5db0639f3
OroshiX <oroshix@users.noreply.github.com>
2019-04-30 01:37:45 +0200

    [FCT] Door for ending opens when collected 1 part => changed prefab arch to contain door

--- Assets/Scripts/ArchEnd.cs
+++ Assets/Scripts/ArchEnd.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 public class ArchEnd : MonoBehaviour {
     [SerializeField] private GameEvent dieEvent;
 
+    [SerializeField] private Animator animatorDoor;
+    private static readonly int OPEN = Animator.StringToHash("open");
+
     private void OnTriggerEnter(Collider other) {
         if (!other.CompareTag("Player")) return;
         // We won!
         dieEvent.sentBool = true;
         dieEvent.Raise();
+    }
+
+    public void openDoor() {
+        animatorDoor.SetTrigger(OPEN);
     }
 }
