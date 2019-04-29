b9adbd8434fade47e9506686eb4ff83b41dd83e2
OroshiX <oroshix@users.noreply.github.com>
2019-04-30 00:15:50 +0200

    [FCT] End game scene + level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    public void goToEndGame() {
+        SceneManager.LoadScene("EndGameScene");
+    }
+
+    public void replay() {
+        SceneManager.LoadScene("Final_scene");
+    }
+}
