ec4cfd9e8f0d18e5e98f55106bb60c3869ab766a
Xavier Chalut <xavier.chalut@gmail.com>
2019-04-29 21:43:39 +0200

    [BUG] Let's move the fenwick when it's slow

--- Assets/Scripts/FenwickIA.cs
+++ Assets/Scripts/FenwickIA.cs
@@ -131,37 +131,39 @@ public class FenwickIA :
     void FixedUpdate()
     {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
-
         Vector3 l_currentVelocity = agent.velocity;
-        Vector3 l_orientation = Vector3.Normalize(l_currentVelocity);
+        if (agent.velocity.magnitude > 0.1)
+        {
+            Vector3 l_orientation = Vector3.Normalize(l_currentVelocity);
 
-        //Contrain velocity direction to follow only one axis at same time
-        Vector3 l_correctOrientation = l_orientation;
-        if (Mathf.Abs(l_orientation.x) >= Mathf.Abs(l_orientation.z))
-        {
-            l_correctOrientation = new Vector3(l_orientation.x, l_orientation.y, 0);
-        }
-        if (Mathf.Abs(l_orientation.z) > Mathf.Abs(l_orientation.x))
-        {
-            l_correctOrientation = new Vector3(0, l_orientation.y, l_orientation.z);
-        }
-
-        //Manual management of velocity (override Unity physique !)
-        Vector3 l_targetDirection = agent.steeringTarget - this.transform.position;
-        if (Vector3.Dot(this.transform.forward, l_targetDirection) < 0)
-        {
-            if(agent.velocity.magnitude > 0.1)
+            //Contrain velocity direction to follow only one axis at same time
+            Vector3 l_correctOrientation = l_orientation;
+            if (Mathf.Abs(l_orientation.x) >= Mathf.Abs(l_orientation.z))
             {
-                agent.velocity = agent.velocity - (Vector3.Normalize(agent.velocity) * Mathf.Clamp(agent.velocity.magnitude, 0, 1f));
+                l_correctOrientation = new Vector3(l_orientation.x, l_orientation.y, 0);
+            }
+            if (Mathf.Abs(l_orientation.z) > Mathf.Abs(l_orientation.x))
+            {
+                l_correctOrientation = new Vector3(0, l_orientation.y, l_orientation.z);
+            }
+
+            //Manual management of velocity (override Unity physique !)
+            Vector3 l_targetDirection = agent.steeringTarget - this.transform.position;
+            if (Vector3.Dot(this.transform.forward, l_targetDirection) < 0)
+            {
+                if (agent.velocity.magnitude > 0.1)
+                {
+                    agent.velocity = agent.velocity - (Vector3.Normalize(agent.velocity) * Mathf.Clamp(agent.velocity.magnitude, 0, 1f));
+                }
+                else
+                {
+                    agent.velocity = Vector3.Normalize(l_targetDirection) * m_currentForceSpeed;
+                }
             }
             else
             {
-                agent.velocity = Vector3.Normalize(l_targetDirection) * m_currentForceSpeed;
+                agent.velocity = l_correctOrientation * m_currentForceSpeed;
             }
-        }
-        else
-        {
-            agent.velocity = l_correctOrientation * m_currentForceSpeed;
         }
     }
     /********  PUBLIC           ************************/
