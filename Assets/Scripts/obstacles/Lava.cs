51532a3ef97201109d527c175e19ff0c21450c68
Xavier Chalut <xavier.chalut@gmail.com>
2019-04-30 02:54:08 +0200

    [FCT] Lava burns ! (maybe.. not tested yet ¯\_(ツ)_/¯ )

---
+++ Assets/Scripts/obstacles/Lava.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace obstacles
+{
+    public class Lava : MonoBehaviour
+    {
+
+        [SerializeField]
+        private float hurtAmount = 5f;
+        private float m_cooldown = 0.5f;
+
+        public GameObject robo;
+        private float m_cooldownTimer = 0.5f;
+
+        private void Update()
+        {
+            if(m_cooldownTimer > 0 )
+            {
+                m_cooldownTimer -= Time.deltaTime;
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (m_cooldown <= 0)
+            {
+                if (other.transform.gameObject.CompareTag("Player"))
+                {
+                    behavior.Life l_roboScript = robo.GetComponent<behavior.Life>();
+                    l_roboScript.lose(hurtAmount);
+                    m_cooldownTimer = m_cooldown;
+                }
+            }
+        }
+    }
+}
