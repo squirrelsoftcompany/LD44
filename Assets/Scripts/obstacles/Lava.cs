8a358c52558d438f365f8dff12231149df93e92e
Xavier Chalut <xavier.chalut@gmail.com>
2019-04-30 03:18:29 +0200

    [FCT] Fix lava

--- Assets/Scripts/obstacles/Lava.cs
+++ Assets/Scripts/obstacles/Lava.cs
@@ -9,9 +9,9 @@ namespace obstacles
 
         [SerializeField]
         private float hurtAmount = 5f;
-        private float m_cooldown = 0.5f;
+        private float m_cooldown = 0.0f;
 
-        public GameObject robo;
+        //public GameObject robo;
         private float m_cooldownTimer = 0.5f;
 
         private void Update()
@@ -26,11 +26,16 @@ namespace obstacles
         {
             if (m_cooldown <= 0)
             {
-                if (other.transform.gameObject.CompareTag("Player"))
+                GameObject l_roboTrack = other.transform.gameObject;
+                if (l_roboTrack.CompareTag("Player"))
                 {
-                    behavior.Life l_roboScript = robo.GetComponent<behavior.Life>();
-                    l_roboScript.lose(hurtAmount);
-                    m_cooldownTimer = m_cooldown;
+                    behavior.Life l_roboScript = l_roboTrack.transform.Find("Body").GetComponent<behavior.Life>();
+                    if(l_roboScript)
+                    {
+                        l_roboScript.lose(hurtAmount);
+                        m_cooldownTimer = m_cooldown;
+                    }
+                    
                 }
             }
         }
