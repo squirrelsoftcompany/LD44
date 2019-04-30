37069dd354c8dc68b604c99e8406452055fc5205
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-04-30 23:37:21 +0200

    [QA] remove Debug.log

--- Assets/Scripts/behavior/HitManagement.cs
+++ Assets/Scripts/behavior/HitManagement.cs
@@ -12,17 +12,6 @@ namespace behavior {
             Hurtful hurtful = other.transform.GetComponent<Hurtful>();
             if (hurtful)
             {
-                List<ContactPoint> contacts = new List<ContactPoint>();
-                other.GetContacts(contacts);
-                foreach(ContactPoint contact in contacts)
-                {
-                    string str = "other: " + contact.otherCollider.name
-                            + "\n - Parent: " + contact.otherCollider.transform.parent.name
-                            + "\nthis: " + contact.thisCollider.name
-                            + "\n - Parent: " + contact.thisCollider.transform.parent.name;
-                    Debug.Log(str);
-                }
-
                 other.GetContact(0).thisCollider.GetComponent<Life>().lose(hurtful.HurtAmount);
             }
         }
